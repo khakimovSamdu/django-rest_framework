502d9f96588bdb63572e84b4dc2e5f60b94e7f5b
khakimovSamdu <allamurodxakimov@gmail.com>
2024-03-28 21:22:57 +0500

    response

--- api/urls.py
+++ api/urls.py
@@ -1,4 +1,7 @@
 from django.urls import path
+from .views import HomePage, home
 urlpatterns = [
+    path('get/', HomePage.as_view(), name="homepage"),
+    path('home/', home, name='home'),
     
 ]
--- api/views.py
+++ api/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django.views import View
+from django.http import HttpResponse, HttpRequest, JsonResponse
+from rest_framework import request
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from rest_framework import status
 
 # Create your views here.
+class HomePage(View):
+    def get(self, request: HttpRequest):
+        return JsonResponse({"Get_method":"Get method ishlamoqda"})
+
+@api_view(['GET'])
+def home(request: request):
+    print(request)
+    print(request.query_params)
+    data = {
+        "message":"Hello Hello, world. You're at the polls Homepage"
+    }
+    return Response(data, status=status.HTTP_200_OK)
